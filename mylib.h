d0535878813bb3e45467e6de2084c4d107931e23
nh283483-byte <nh283483@gmail.com>
2025-10-23 16:10:06 +0700

    Add new function declarations to mylib.h

--- mylib.h
+++ mylib.h
@@ -1,6 +1,12 @@
 #include<stdio.h>
 void xemthang();
 void namnhuan();
-
+void giaiPTBac2();
+void xepLoai();
+void tinhTienDien();
+void inBangCuChuong();
+void tinhGiaiThua();
+void hienthi();
+void nhapTu1DenN();
 void inFibonacci();
 void inCayThong();
