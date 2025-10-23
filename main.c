1de58586122e2e9b8b5dfd0923655966ebd18aa9
nh283483-byte <nh283483@gmail.com>
2025-10-23 16:09:06 +0700

    Update main.c

--- main.c
+++ main.c
@@ -4,14 +4,18 @@ void menu(){
     printf("*** MENU ***\n");
     printf("1- Xem ngay cua thang\n");
     printf("2- Kiem tra nam nhuan\n");
-
-    printf("10- In day Fibonacci\n");
-    printf("11- In cay thong\n");
-
+    printf("3- Giai Phuong trinh bac 2\n");
+    printf("4- Xep loai dua tren diem trung binh\n");
+    printf("5- Tinh tien dien\n");
+    printf("6- Bang cuu chuong\n");
+    printf("7- Tinh giai thua\n");
+    printf("8- Hien thi\n");
+    printf("9- Nhap tu 1 den N\n");
+    printf("10- Fibonacci\n");
+    printf("11- in Cay Thong bang dau *\n");
     printf("------------------\n");
     printf("chon STT chuc nang: \n");
     printf("------------------\n");
-
     int c;
     scanf("%d",&c);
 
@@ -25,16 +29,42 @@ void menu(){
         namnhuan();
         menu();
         break;
-
-    case 10: 
-        inFibonacci(); 
-        menu(); 
+    case 3:
+        giaiPTBac2();
+        menu();
         break;
-    case 11: 
-        inCayThong(); 
-        menu(); 
+    case 4:
+        xepLoai();
+        menu();
         break;
-        
+    case 5:
+        tinhTienDien();
+        menu();
+        break;
+    case 6:
+        inBangCuChuong();
+        menu();
+        break;
+    case 7:
+        tinhGiaiThua();
+        menu();
+        break;
+    case 8:
+        hienthi();
+        menu();
+        break;
+    case 9:
+        nhapTu1DenN();
+        menu();
+        break;
+    case 10:
+        inFibonacci();
+        menu();
+        break;
+    case 11:
+        inCayThong();
+        menu();
+        break;
     default:
         menu();
         break;
